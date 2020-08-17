411e77aab0993eb16207564bc68baeff0127fd9e
Tharin <tharin2501@hotmail.no>
2020-08-17 15:55:11 +0200

    styling og card component

--- src/components/Repository.vue
+++ src/components/Repository.vue
@@ -1,62 +1,59 @@
-<!--
 <template>
-    <div id="card">
-
-        <div id="mycard-header">
-
-        </div>
-
-        <div id="card-body">
-
-            <div id="card-body-header">
-                <h1>48-hour code: Movie rating application</h1>
-            </div>
-            <div id="card-body-description pt-3 mt-3">
-                <p>
-                    This web application is a a movie rating website like IMDb. The application shows a list of movies,
-                    where logged-in users can star (1 to 5) them, and write short reviews.
-                </p>
-                <div id="row mt-3 pl-3">
-                    <button type="button" id="btn btn-primary">
-                        <a>View code</a>
-                    </button>
+    <div>
+        <div class="w-100 mt-5"></div>
+        <!-- v-for executes a new line after each loop, use float-left removes that-->
+        <div v-for="item in list" v-bind:key="item.id" class="float-right p-5 mt-5">
+            <div id="card">
+                <div id="mycard-header">
+                    <img src="../assets/github_logo.png" class="w-50 float-right " alt="img not found">
                 </div>
-            </div>
-
-            <div id="card-body-footer">
-                <div id="card-header-mask">
-                    <div id="card-header-date">
-                        <div id="card-header-date-day">12</div>
-                        <div id="card-header-date-month">May</div>
+                <div id="card-body">
+                    <div id="card-body-header">
+                        <b-row>
+                            <b-col><h3>{{item.name}}</h3></b-col>
+                            <div class="w-100 mt-4"></div>
+                            <b-col>
+                                <div id="card-header-date">{{item.language}}</div>
+                            </b-col>
+                        </b-row>
+                    </div>
+                    <div id="card-body-description">
+                        <p>
+                            {{item.description}}
+                        </p>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
 
+</template>
+-->
+<!--
+<template>
+    <div>
+
+        <div v-for="item in list" v-bind:key="item.id">
+            <div id="card">
+            {{repoImg}}
+            <div id="mycard-header"></div>
+                {{item.name}}
+            <div>{{item.description}}</div>
+            <div>{{item.language}}</div>
+            <div>{{item.date}}</div>
+            <div>{{item.created_at}}</div>
             </div>
         </div>
     </div>
 </template>
 -->
-<template>
-    <div>
-        <!-- https://vuejs.org/v2/guide/list.html -->
-        <div v-for="item in list" v-bind:key="item.id">
-            <div>{{repoImg}}</div>
-            <div>{{item.name}}</div>
-            <div>{{item.description}}</div>
-            <div>{{item.language}}</div>
-            <div>{{item.date}}</div>
-            <div>{{item.created_at}}</div>
-        </div>
-    </div>
-</template>
-
 <script>
     import axios from "axios"
 
     export default {
         name: 'Repository',
         data() {
-            return {list: undefined,repoImg : "default github style image for all repos"}
+            return {list: undefined, repoImg: "default github style image for all repos"}
         },
         mounted() {
             axios
@@ -67,19 +64,18 @@
                 }))
         }
     }
-
 </script>
 
 <style>
 
     #card {
-        height: 22rem;
+        height: 20rem;
         width: 16rem;
         padding: 0;
         transform: translate(0, -8%);
-        background-color: blue;
-        margin: auto;
-        margin-top: 15%;
+        margin-top: 50%;
+
+
     }
 
     #mycard-header {
@@ -87,6 +83,8 @@
         min-height: 175px;
         max-height: 175px;
         transition: all .3s ease-in-out;
+        background: rgb(89,183,219);
+        background: linear-gradient(0deg, rgba(89,183,219,1) 0%, rgba(108,140,226,1) 100%);
     }
 
     .card:hover #mycard-header {
@@ -97,28 +95,31 @@
 
     /* card bak */
     #card-body {
-        background-color: white;
+        background: rgb(89,183,219);
         border-radius: 0 0 1px 1px;
         padding: 0 26px 26px 26px;
         box-shadow: 0 1px 2px rgba(0, 0, 0, .2);
-
+        height: 70%;
     }
 
     /* card foran */
     #card-body-header {
         position: absolute;
         left: 0;
-        background-color: white;
+        background: rgb(89,183,219);
         padding-top: 12%;
         transition: all .3s ease-in-out;
-        margin-top: -5%;
         width: 100%;
-        height: 90%;
+        height: 63%;
+        text-align: center;
+        color: #FFFFFF;
+        text-transform: uppercase;
+        text-shadow: 2px 2px blueviolet;
     }
 
     #card-body-description {
         opacity: 0;
-        color: #757B82;
+        color: white;
         line-height: 110%;
         padding-top: 5%;
         transition: all .2s ease-in-out;
@@ -132,75 +133,20 @@
     /* Hvor høyt kort foran beveger seg opp når hover */
     #card:hover #card-body-header {
         margin-top: -78%; /*-39.5vh hvor høyt den går*/
-        height: 200px; /* høyden på hover kort */
+        /* høyden på hover kort */
     }
 
-
-    #card-body-footer {
-        display: inline;
-
-
-        z-index: 10;
-
-        margin-right: 50%;
-
-        font-size: 14px;
-
-        color: #9FA5A8;
-
-    }
-
-    #circle {
-        color: white;
-
-        height: 45px;
-        width: 45px;
-        background-color: orange;
-        border-radius: 50%;
-        display: inline-block;
-        margin-left: 85%;
-    }
 
     #card-header-date {
-
-        float: right;
-
-
-        border-radius: 50%;
-
-        font-weight: bold;
-
-        padding: 12px 15px;
-
-        line-height: 15px;
-
-    }
-
-
-    #card-header-date-day {
-
-        font-size: 18px;
-
-    }
-
-
-    #card-header-date-month {
-
-        font-size: 11px;
-
-    }
-
-    #card-header-date, .card-body-header-category {
-
         text-transform: uppercase;
-
-        background-color: #EF5A31;
-
-        color: #FFF;
-
+        background-color: gold;
+        color: whitesmoke;
         font-weight: bold;
-
         text-align: center;
+        padding: 16px 8px;
+        line-height: 15px;
+        font-size: 15px;
+        text-shadow: none;
 
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import { BootstrapVue } from 'bootstrap-vue'
 Vue.config.productionTip = false;
 
 // Every Vue application starts by creating a new Vue instance, its the entrypoint of  our app
 new Vue({
   render: h => h(App),
 }).$mount('#app');
+
+// Install BootstrapVue
+Vue.use(BootstrapVue);
